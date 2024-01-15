2a9d96dd74b725ec760ea0e481a22939d96ee57d
Victor Frye <victorfrye@outlook.com>
2024-01-15 09:11:14 -0500

    feat: continued init client

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,28 +1,11 @@
-using VictorFrye.MicrosoftGraveyard.Client.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using VictorFrye.MicrosoftGraveyard.Client;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddHttpClient()
-    .AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddHttpClient();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
+await builder.Build().RunAsync();
