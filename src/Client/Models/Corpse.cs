b9b68cdd747120666f7b53d285c40476afe6e9db
Victor Frye <victorfrye@outlook.com>
2024-01-17 08:50:59 -0500

    feat: use rich domain model for corpses

--- src/Client/Models/Corpse.cs
+++ src/Client/Models/Corpse.cs
@@ -1,31 +1,49 @@
+using System.Text;
+
 namespace VictorFrye.MicrosoftGraveyard.Client.Models;
 
-public class Corpse
+public class Corpse(string slug, string name, string? subtitle, DateOnly? birthDate, DateOnly deathDate, string description, string link)
 {
-    public string Slug { get; set; }
-    public string Name { get; set; }
-    public string? Subtitle { get; set; }
-    public DateOnly? BirthDate { get; set; }
+    public string Slug { get; init; } = slug;
+    public string Name { get; init; } = name;
+    public string? Subtitle { get; init; } = subtitle;
+    public DateOnly? BirthDate { get; init; } = birthDate;
 
-    public DateOnly DeathDate { get; set; }
-    public string Description { get; set; }
-    public string Link { get; set; }
+    public DateOnly DeathDate { get; init; } = deathDate;
+    public string Description { get; init; } = description;
+    public string Link { get; init; } = link;
 
-    public Corpse() { }
+    public bool IsDead() => DeathDate <= DateOnly.FromDateTime(DateTime.Now);
 
-    public Corpse(string slug, string name, string? subtitle, DateOnly? birthDate, DateOnly deathDate, string description, string? link)
+    public int? GetAge()
     {
-        Slug = slug;
-        Name = name;
-        Subtitle = subtitle;
-        BirthDate = birthDate;
-        DeathDate = deathDate;
-        Description = description;
-        Link = link;
+        if (BirthDate is null)
+        {
+            return null;
+        }
+
+        var age = DeathDate.Year - BirthDate.Value.Year;
+
+        if (DeathDate < BirthDate.Value.AddYears(age))
+        {
+            age--;
+        }
+
+        return age;
     }
 
-    public bool IsDead()
+    public string GetExpectedDeathDate() => $"{DeathDate:MMMM yyyy}";
+
+    public string GetLifeDates() => BirthDate is null ? $"{DeathDate:yyyy}" : $"{BirthDate.Value:yyyy} - {DeathDate:yyyy}";
+
+    public string GetFullName() => Subtitle is null ? Name : $"{Name} ({Subtitle})";
+
+    public string GetObituary()
     {
-        return DeathDate <= DateOnly.FromDateTime(DateTime.Now);
+        var obituary = new StringBuilder();
+
+        obituary.Append($"{Name} {(IsDead() ? "was" : "is")} {Description}.");
+
+        return obituary.ToString();
     }
 }
