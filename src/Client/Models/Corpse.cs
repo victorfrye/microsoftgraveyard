94cd067219443b98c6efb75ac85bf1729e618315
Victor Frye <victorfrye@outlook.com>
2024-01-19 13:05:27 -0500

    feat: flesh out obituary generator

--- src/Client/Models/Corpse.cs
+++ src/Client/Models/Corpse.cs
@@ -13,24 +13,9 @@ public class Corpse(string slug, string name, string? subtitle, DateOnly? birthD
     public string Description { get; init; } = description;
     public string Link { get; init; } = link;
 
-    public bool IsDead() => DeathDate <= DateOnly.FromDateTime(DateTime.Now);
+    private DateOnly _now = DateOnly.FromDateTime(DateTime.Now);
 
-    public int? GetAge()
-    {
-        if (BirthDate is null)
-        {
-            return null;
-        }
-
-        var age = DeathDate.Year - BirthDate.Value.Year;
-
-        if (DeathDate < BirthDate.Value.AddYears(age))
-        {
-            age--;
-        }
-
-        return age;
-    }
+    public bool IsDead() => DeathDate <= _now;
 
     public string GetExpectedDeathDate() => $"{DeathDate:MMMM yyyy}";
 
@@ -42,8 +27,54 @@ public class Corpse(string slug, string name, string? subtitle, DateOnly? birthD
     {
         var obituary = new StringBuilder();
 
+        if (IsDead()) {
+            var timeDead = GetAge(DeathDate, _now);
+            var message = timeDead.Value.Age == 0 ? "today" : $"{timeDead.Value.Age} {timeDead.Value.Period} ago";
+            obituary.Append($"Killed by Microsoft {message}, ");
+        } else {
+            var execution = GetAge(_now, DeathDate);
+            obituary.Append($"To be killed by Microsoft in {execution.Value.Age} {execution.Value.Period}, ");
+        }
+
         obituary.Append($"{Name} {(IsDead() ? "was" : "is")} {Description}.");
 
+        if (IsDead() && BirthDate is not null)
+        {
+            var timeLived = GetAge(BirthDate.Value, DeathDate);
+            obituary.Append($" It was {timeLived.Value.Age} {timeLived.Value.Period} old.");
+        }
+
         return obituary.ToString();
+    }
+
+    private static (int Age, string Period)? GetAge(DateOnly start, DateOnly end)
+    {
+        var years = end.Year - start.Year;
+
+        if (end.Month < start.Month || (end.Month == start.Month && end.Day < start.Day))
+        {
+            years--;
+        }
+
+        if (years >= 1)
+        {
+            return (years, years == 1 ? "year" : "years");
+        }
+
+        var months = end.Month - start.Month + (12 * (end.Year - start.Year));
+
+        if (end.Month == start.Month && end.Day < start.Day)
+        {
+            months--;
+        }
+
+        if (months >= 1)
+        {
+            return (months, months == 1 ? "month" : "months");
+        }
+
+        var days = end.Day - start.Day;
+
+        return (days, days == 1 ? "day" : "days");
     }
 }
