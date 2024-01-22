7d923919e031035e0bd76fdb258de15a03b75b3b
Victor Frye <victorfrye@outlook.com>
2024-01-21 21:27:51 -0500

    feat: responsive list elements (#19)

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using VictorFrye.MicrosoftGraveyard.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
