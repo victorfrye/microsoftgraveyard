87a4d049740fade85ac29712a6e39b7d501e96ce
Victor Frye <victorfrye@outlook.com>
2025-05-16 11:47:21 +0000

    chore: add node and gh cli features to devcontainer

--- .devcontainer/scripts/install.sh
+++ .devcontainer/scripts/install.sh
@@ -1,12 +1,9 @@
-echo "Installing Node.js LTS and npm packages..."
-
-. ${NVM_DIR}/nvm.sh && nvm install --lts
-
-pushd src/WebClient
-npm install
-popd
-echo "Installation of Node.js LTS and npm packages complete."
-
 echo "Restoring .NET dependencies..."
 dotnet restore
 echo "Restoration of .NET dependencies complete."
+
+echo "Installing npm dependencies..."
+pushd src/WebClient
+npm install
+popd
+echo "Installation of npm dependencies complete."
